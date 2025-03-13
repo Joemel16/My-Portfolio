f97efaa2cf3bb179a9844d94d324c9ccc23eab69
Joemel16 <118241096+Joemel16@users.noreply.github.com>
2025-03-13 09:43:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// This can be expanded with any interactive elements you wish to add.
+// For now, we can add a simple script to handle the form submission:
+
+function submit_form() {
+    alert("Your message has been sent!");
+    // Normally, you'd send the form data to a server here
+}
